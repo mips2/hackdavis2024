92b173251776ace013d616350cc80202e5d8f255
Liam Morgan <95554190+mips2@users.noreply.github.com>
2024-04-27 16:23:15 -0700

    commit

--- front/src/components/login/Login.js
+++ front/src/components/login/Login.js
@@ -1,17 +1,39 @@
 import React from 'react'
+import './login.css'
+import axios from 'axios';
+import { useState } from 'react';
 
 const Login = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          await axios.post('http://127.0.0.1:5000/login', {
+            username,
+            password,
+          });
+          // Optionally, handle success (e.g., show a success message)
+          console.log('Post created successfully');
+        } catch (error) {
+          // Optionally, handle error (e.g., show an error message)
+          console.error('Error creating post:', error);
+        }
+      };
+
   return (
     <div class="container">
-    <form class="login-form" action="#" method="post">
+    <form class="login-form" onSubmit={handleSubmit}>
         <h2>Login</h2>
         <div class="form-group">
             <label for="username">Username</label>
-            <input type="text" id="username" name="username" required />
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
         </div>
         <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" id="password" name="password" required />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
         </div>
         <button type="submit">Login</button>
     </form>
