cc712b1a5ef4074a4d52ceaea1dce688f59930ee
Nicolas Hak <nicolas.l.hak@gmail.com>
2024-04-27 16:00:54 -0700

    routing

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Login from './pages/login';
+import Home from './pages/home';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';  // Import Axios
@@ -6,36 +9,14 @@ import axios from 'axios';  // Import Axios
 function App() {
   const [data, setData] = useState(null);  // State to store response data
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://127.0.0.1:5000/');  // Replace with your backend URL
-        setData(response.data.number);  // Access correct property from response data
-        console.log('Data:', response.data); // Log the data to the console
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          YO WSG <code>yooooooo</code> Number is: {data !== null ? data : 'Loading...'}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element={<Home/>} />
+          <Route path="/login" element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
