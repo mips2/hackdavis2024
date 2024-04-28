99a76c1987d85578a5335a5625715f2e9f0f3096
luukjanssen9 <luukjanssen9@outlook.com>
2024-04-27 19:29:45 -0700

    finished registeration page

--- front/src/components/register/register.js
+++ front/src/components/register/register.js
@@ -30,28 +30,31 @@ const Register = ({onLogin,isLoggedIn}) => {
       };
 
   return (
-    <div class="register-container">
-        <form class="register-form">
-            <h2>Register New Account</h2>
-            <div class="form-group">
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" required />
-            </div>
-            <div class="form-group">
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" required />
-            </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required />
-            </div>
-            <div class="form-group">
-                <label for="confirm-password">Confirm Password:</label>
-                <input type="password" id="confirm-password" name="confirm-password" required />
-            </div>
-            <button type="submit">Register</button>
-        </form>
+    <div class="background">
+        <div class="register-container">
+            <form class="register-form">
+                <h2>Register New Account</h2>
+                <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" name="name" required />
+                </div>
+                <div class="form-group">
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username" required />
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <div class="form-group">
+                    <label for="confirm-password">Confirm Password:</label>
+                    <input type="password" id="confirm-password" name="confirm-password" required />
+                </div>
+                <button type="submit">Register</button>
+            </form>
+        </div>
     </div>
+    
   )
 }
 
