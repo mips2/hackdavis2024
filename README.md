e18e36921a00d448dc29d84be8a4920fed2d3fc8
luukjanssen9 <luukjanssen9@outlook.com>
2024-04-28 12:55:46 -0700

    cleaned up file structure and started report

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-temp
+# Emply
+
+Emply is //todo - explain what the website is
+
+## Backend:
+
+### App.js:
+
+`App.js` is responsible for managing the states for the job listings. The state is initialized as an empty array and filled updated when the data is fetched from the server. Data is fetched from the backend by using the `fetchData()` function, which makes a GET request to the server using axious to get the job data. `App.js` also ensures user login handling which takes a username and password as arguments. If the username and password match an existing user in the database, the user gets sent to the home page. It updates the localStorage to reflect that the user is logged in, stores the username, and whether the user is a company or not. `App.js` is also responsible for the routing: It defines a Routes element that lists all the possible routes and the components that should be rendered for each path. For example:
+The root path `("/")` renders the Home component, passing the jobs data as a prop. Other paths render their respective components like Applications, Profile, Logout, Register, Application, ProfileUpdate, and Applypage. Each route is associated with a specific component which handles the user interface and functionality for that part of the application. This modular approach helps in managing the application efficiently and makes it easier to maintain and update.
+
+### App.css/ App.test.js:
+
+`App.css/ App.test.js` are both not used since `App.js` doesn't need to be styled. `App.test.js` is kept in case test need to be added in the future.
+
+## Frontend:
+
+### Header:
+
+`header.js/.css` is the header structure that is applied to other files like `profile.js` and `applications.js`
+
+### Login:
+
+`login.js/.css` is the page that the user starts on and once logged in, gets routed to the main page. If the user has never made an account they have to register to the register page. 
+
+### Register:
+
+`register.js/.css` is a simple registration page that saves the users information such as: email, username, password, name, phone number, etc. This information is saved in the users profile.
+
+### Home:
+
+`Home.js/.css` is the main page that the user gets redirected to once they succesfully log in. It serves as the main user interface for displaying job listings on a website. This component integrates several functionalities, including a search bar that allows users to filter jobs based on keywords and location, and a series of buttons for additional filtering options like job type and job category. Each job is presented using the JobCard component which displays details such as the company logo, job title, and location, and includes a "Quick Apply" button for easy application. Additionally, the component contains logic to redirect users who are not logged in to the login page, ensuring that only authenticated users can access the job listings.
+
+### Profile:
+
+`profile.js/.css, ProfileComponent.js, ProfileUpdate.js/.css` get the user's information from the backend and display the user's information, which can be edited real time. 
+
+### Logout:
+
+`logout.js` resets the user's logged in status when clicked, and route the user to the log in page.
+
+### My Applications:
+
+`applications.js/.css` lists all the applications that the user completed, taken from the server. Applications are sent to this page once completed.
+
+### Application
+
+`application.js/.css` is the page that the user gets routed to when they click apply on a company's post. This page will save your information to the server once completed.
--- front/src/components/home/Home.js
+++ front/src/components/home/Home.js
@@ -4,119 +4,141 @@ import CompanyPage from '../company/company';
 import Header from '../header';
 import Login from '../login/Login';
 import './home.css';
+
+// Component for displaying a job card
 const JobCard = ({ job }) => {
-
+  // Hook for navigation
   const navigate = useNavigate();
-return (
-<div className ="job-card-container">
-<article className="job-card">
-  <div className="company-logo">
-    <img src={job.logo} alt={`${job.company} logo`} />
-  </div>
-  <div className="job-details">
-    <div className="job-header">
-      <h3 className="job-title">{job.title}</h3>
+
+  return (
+    <div className="job-card-container">
+      <article className="job-card">
+        {/* Company logo */}
+        <div className="company-logo">
+          <img src={job.logo} alt={`${job.company} logo`} />
+        </div>
+        {/* Job details */}
+        <div className="job-details">
+          <div className="job-header">
+            <h3 className="job-title">{job.title}</h3>
+          </div>
+          <div className="job-meta">
+            {/* Company and location */}
+            <p className="company-location">
+              {job.company}
+              <br />
+              {job.location}
+            </p>
+          </div>
+        </div>
+        {/* Quick apply button */}
+        <section className="quickcont">
+          <button className="quick-apply-btn" onClick={() => navigate('./applypage')}>
+            Quick Apply
+          </button>
+        </section>
+      </article>
     </div>
-    <div className="job-meta">
-      <p className="company-location">
-        {job.company}
-        <br />
-        {job.location}
-      </p>
+  );
+};
+
+// Component for the search bar
+const SearchBar = ({ onSearchChange, onLocationChange }) => (
+  <div className="search-bar">
+    {/* Search input */}
+    <div className="search-input">
+      <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/dac0bffb10fa8b9b03590bb619ad64d0747a7bc9edb75820fe380bf14b252864?apiKey=c7598dd2036b4baf9e49409c4a6781c6&" alt="Search icon" />
+      <label htmlFor="search-jobs" className="visually-hidden">
+        Search for companies or positions
+      </label>
+      <input
+        type="text"
+        id="search-jobs"
+        placeholder="Search for companies or positions..."
+        aria-label="Search for companies or positions"
+        onChange={(e) => onSearchChange(e.target.value)}
+      />
+    </div>
+    {/* Location input */}
+    <div className="location-input">
+      <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/3aa7725667142a53b63a4754839f021dbff43134149b29eb95e1e8b6f5452fdf?apiKey=c7598dd2036b4baf9e49409c4a6781c6&" alt="Location icon" />
+      <label htmlFor="location" className="visually-hidden">
+        City, State, or Zip Code
+      </label>
+      <input
+        type="text"
+        id="location"
+        placeholder="City, State, or Zip Code"
+        aria-label="City, State, or Zip Code"
+        onChange={(e) => onLocationChange(e.target.value)}
+      />
     </div>
   </div>
-  <section className="quickcont">
-    <button className="quick-apply-btn" onClick={() => navigate('./applypage')}>Quick Apply</button>
-  </section>
-</article>
-</div>
-);
-}
-const SearchBar = ({ onSearchChange,onLocationChange }) => (
-<div className="search-bar">
-  <div className="search-input">
-    <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/dac0bffb10fa8b9b03590bb619ad64d0747a7bc9edb75820fe380bf14b252864?apiKey=c7598dd2036b4baf9e49409c4a6781c6&" alt="Search icon" />
-    <label htmlFor="search-jobs" className="visually-hidden">
-      Search for companies or positions
-    </label>
-    <input
-      type="text"
-      id="search-jobs"
-      placeholder="Search for companies or positions..."
-      aria-label="Search for companies or positions"
-      onChange={e => onSearchChange(e.target.value)}
-    />
-  </div>
-  <div className="location-input">
-    <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/3aa7725667142a53b63a4754839f021dbff43134149b29eb95e1e8b6f5452fdf?apiKey=c7598dd2036b4baf9e49409c4a6781c6&" alt="Location icon" />
-    <label htmlFor="location" className="visually-hidden">
-      City, State, or Zip Code
-    </label>
-    <input
-      type="text"
-      id="location"
-      placeholder="City, State, or Zip Code"
-      aria-label="City, State, or Zip Code"
-      onChange={e => onLocationChange(e.target.value)}
-      
-    />
-  </div>
-</div>
 );
 
+// Component for filter button
 const FilterButton = ({ children, onClick }) => (
   <button className="filter-btn" onClick={onClick}>
     {children}
   </button>
 );
-const Home = ({ jobs }) => {
-  console.log(jobs);
-  const [displayedJobs, setDisplayedJobs] = useState(jobs);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [locationTerm, setLocationTerm] = useState('');
 
-  console.log(displayedJobs);
-  
+// Main component for the home page
+const Home = ({ jobs }) => {
+  // State for displayed jobs
+  const [displayedJobs, setDisplayedJobs] = useState(jobs);
+  // State for search term
+  const [searchTerm, setSearchTerm] = useState('');
+  // State for location term
+  const [locationTerm, setLocationTerm] = useState('');
+  // State for active filters
   const [activeFilters, setActiveFilters] = useState({
     fullTime: false,
     partTime: false,
     softwareEngineering: false,
     computerScience: false
   });
+
+  // Effect hook to update displayed jobs when jobs change
   useEffect(() => {
     setDisplayedJobs(jobs);
   }, [jobs]);
 
+  // Effect hook to filter jobs when active filters change
   useEffect(() => {
     filterJobs();
-  }, [activeFilters]); 
+  }, [activeFilters]);
 
+  // Effect hook to filter jobs based on search term and location term
   useEffect(() => {
-    const filteredJobs = jobs.filter(job => 
+    const filteredJobs = jobs.filter(job =>
       job.company.toLowerCase().includes(searchTerm.toLowerCase()) &&
       job.location.toLowerCase().includes(locationTerm.toLowerCase())
     );
     setDisplayedJobs(filteredJobs);
-  }, [searchTerm, locationTerm,jobs]);
+  }, [searchTerm, locationTerm, jobs]);
 
+  // Function to filter jobs based on active filters
   const filterJobs = () => {
     let filteredJobs = jobs;
-    
-    if(activeFilters.fullTime) {
+
+    if (activeFilters.fullTime) {
       filteredJobs = filteredJobs.filter(job => job.type === 'Full-Time');
     }
-    if(activeFilters.partTime) {
+    if (activeFilters.partTime) {
       filteredJobs = filteredJobs.filter(job => job.type === 'Part-Time');
     }
-    if(activeFilters.softwareEngineering) {
+    if (activeFilters.softwareEngineering) {
       filteredJobs = filteredJobs.filter(job => job.Category === 'Software Engineering');
     }
-    if(activeFilters.computerScience) {
+    if (activeFilters.computerScience) {
       filteredJobs = filteredJobs.filter(job => job.Category === 'Computer Science');
     }
-    
+
     setDisplayedJobs(filteredJobs);
-  }
+  };
+
+  // Function to toggle filter
   const toggleFilter = (filterKey) => {
     const isActive = activeFilters[filterKey];
     const updatedFilters = {
@@ -128,55 +150,48 @@ const Home = ({ jobs }) => {
 
     const anyActive = Object.values(updatedFilters).some(status => status);
     if (!anyActive) {
-      setDisplayedJobs(jobs);  
+      setDisplayedJobs(jobs);
     }
   };
 
-  console.log("home");
-  console.log(displayedJobs);
+  // Redirect to login page if not logged in
   if (localStorage.getItem('isLoggedIn') === 'false') {
-      window.location.href = '/login';
-      return <Login/>
+    window.location.href = '/login';
+    return <Login />
   }
 
-
+  // Handlers for search and location change
   const handleSearchChange = (term) => {
     setSearchTerm(term);
   };
   const handleLocationChange = (term) => {
     setLocationTerm(term);
   };
-  
-  // if (localStorage.getItem('isCompany') === 'false'){
-    return (
-      <div>
-        <Header></Header>
-        <div className="app">
-          <main className="main-content">
-            <SearchBar onSearchChange={handleSearchChange} onLocationChange={handleLocationChange} />
-            <div className="filters">
-              <FilterButton onClick={() => toggleFilter('fullTime')}>Full-Time</FilterButton>
-              <FilterButton onClick={() => toggleFilter('partTime')} >Part-Time</FilterButton>
-              <FilterButton onClick={() => toggleFilter('softwareEngineering')}>Software Engineering</FilterButton>
-              <FilterButton onClick={() => toggleFilter('computerScience')}>Computer Science</FilterButton>
-              <FilterButton>Filters - 4</FilterButton>
-            </div>
-            {displayedJobs.map((job) => (
-              <JobCard key={job._id.$oid} job={job} />
-            ))}
-          </main>
-        </div>
-      </div>
-    )
-  // }
-  // else{
-  //   return(
-  //     <div>
-  //       <CompanyPage></CompanyPage>
-  //     </div>
-  //   )
-  // }
 
-}
+  // Render home page
+  return (
+    <div>
+      <Header></Header>
+      <div className="app">
+        <main className="main-content">
+          {/* Search bar */}
+          <SearchBar onSearchChange={handleSearchChange} onLocationChange={handleLocationChange} />
+          {/* Filters */}
+          <div className="filters">
+            <FilterButton onClick={() => toggleFilter('fullTime')}>Full-Time</FilterButton>
+            <FilterButton onClick={() => toggleFilter('partTime')}>Part-Time</FilterButton>
+            <FilterButton onClick={() => toggleFilter('softwareEngineering')}>Software Engineering</FilterButton>
+            <FilterButton onClick={() => toggleFilter('computerScience')}>Computer Science</FilterButton>
+            <FilterButton>Filters - 4</FilterButton>
+          </div>
+          {/* Displayed jobs */}
+          {displayedJobs.map((job) => (
+            <JobCard key={job._id.$oid} job={job} />
+          ))}
+        </main>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
