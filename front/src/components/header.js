477ba343c2731b8cc88a48636f9f6df30db8f431
Minji Yun <minjiyun404@gmail.com>
2024-04-28 01:15:46 -0700

    minjago

--- front/src/components/header.js
+++ front/src/components/header.js
@@ -1,30 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import accountCircleImg from './accountcircle.png';
 import "./header.css";
 
-
 const Header = () => {
-  console.log(localStorage.getItem('isLoggedIn'));
-    return (
+  const [currentTab, setCurrentTab] = useState(""); // State to keep track of current tab
+
+  useEffect(() => {
+    const pathname = window.location.pathname;
+    // Mapping URLs to tab names
+    const tabs = {
+      "/": "Home",
+      "/applications": "My Applications",
+      "/profile": "Profile"
+    };
+    // Setting current tab based on URL
+    setCurrentTab(tabs[pathname]);
+  }, []);
+
+  // Function to handle tab click
+  const handleTabClick = (tabName) => {
+    setCurrentTab(tabName);
+  };
+
+  return (
     <div className='header'>
       <div className="header-content">
         <nav className="navigation">
-          <a href="/" className="nav-link">
+          <a href="/" className={`nav-link ${currentTab === "Home" ? "active" : ""}`} onClick={() => handleTabClick("Home")}>
             Home
           </a>
-          <a href="/applications" className="nav-link2">
+          <a href="/applications" className={`nav-link2 ${currentTab === "My Applications" ? "active" : ""}`} onClick={() => handleTabClick("My Applications")}>
             My Applications
           </a>
-            <p className="jobs-applied"># of jobs applied: 3</p>
-            <p className="applications-left"># of applications left: 5</p>
-            <p className="refresh-time">Refreshing in: 5 days</p>
-
-          <a href="/profile" className="nav-link">
-          <img src={accountCircleImg} alt="User avatar" className="user-avatar" />
+          <p className="jobs-applied"># of jobs applied: 3</p>
+          <p className="applications-left"># of applications left: 5</p>
+          <p className="refresh-time">Refreshing in: 5 days</p>
+          <a href="/profile" className={`nav-link ${currentTab === "Profile" ? "active" : ""}`} onClick={() => handleTabClick("Profile")}>
+            <img src={accountCircleImg} alt="User avatar" className="user-avatar" />
           </a>
         </nav>
+      </div>
     </div>
-  </div>
   );
 }
 
