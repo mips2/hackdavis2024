c1b2ff64016f529ea02b0d0bbfadaea5d17bdc8c
Minji Yun <minjiyun404@gmail.com>
2024-04-27 19:55:21 -0700

    homepage updated

--- front/src/components/header.js
+++ front/src/components/header.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import "./header.css";
-import { Link } from 'react-router-dom';
 
 const Header = () => {
   console.log(localStorage.getItem('isLoggedIn'));
   return (
     <div className='header'>
-      <h2>
-        <Link to='/'>Home</Link>
-        {!localStorage.getItem('isLoggedIn')===true && <Link to='/login'>Login</Link>}
-        {localStorage.getItem('isLoggedIn') && <Link to='/logout'>Logout</Link>}
-        {localStorage.getItem('isLoggedIn') && <Link to='/applications'>My Applications</Link>}
-        {localStorage.getItem('isLoggedIn') && <Link to='/profile'>My Profile</Link>}
-      </h2>
+<div className="header-content">
+            <nav className="navigation">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+              <a href="/applications" className="nav-link">
+                My Applications
+              </a>
+              <p className="jobs-applied"># of jobs applied: 3</p>
+              <p className="applications-left"># of applications left: 5</p>
+            <p className="refresh-time">Refreshing in: 5 days</p>
+            </nav>
+            <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/062ea4dec4c9f51c6ffcfa64fba8324719b720cdbf30baa491508c37d51bcc35?apiKey=c7598dd2036b4baf9e49409c4a6781c6&" alt="User avatar" className="user-avatar" />
+          </div>
     </div>
   );
 }
