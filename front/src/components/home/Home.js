64a38f1ff3112ea2f76e8d995a507c460ce1a682
Liam Morgan <95554190+mips2@users.noreply.github.com>
2024-04-28 04:26:45 -0700

    qwer

--- front/src/components/home/Home.js
+++ front/src/components/home/Home.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import React, { useEffect, useState } from 'react';
 import Header from '../header';
 import Login from '../login/Login';
 import './home.css';
